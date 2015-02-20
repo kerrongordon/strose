53766e839950931f999fe5e900ff245c3ff77d18
kerrongordon <kgpsounds.com@gmail.com>
2015-02-20 16:41:04 -0400

    init

---
+++ page-home.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ Template Name: Home Page 
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+	
+	<?php putRevSlider( "strose" ) ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php get_sidebar( "homepagemain" ); ?>
+
+					</main>
+
+					<?php get_sidebar( "homesidebar" ); ?>						
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
